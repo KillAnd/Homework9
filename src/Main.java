a5b238657344b1986784d76789b276553f3ea8cd
kila1 <kila14081996@gmail.com>
2024-03-19 09:47:28 +0300

    Домашнее задание №9.Задание 1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello work 9!");
+
+        //Task 1
+        System.out.println("Task 1");
+
+        int [] moneyWeek = new int[5];
+        moneyWeek[0] = 112370;
+        moneyWeek[1] = 82370;
+        moneyWeek[2] = 134370;
+        moneyWeek[3] = 56370;
+        moneyWeek[4] = 162370;
+        int sum = 0;
+        for (int i : moneyWeek) {
+            sum+=i;
+        }
+        System.out.println("Сумма трат в месяц составила " + sum + " рублей.");
+
+
+
     }
 }
